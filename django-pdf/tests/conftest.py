fbfd438cd11669d74dca1e801385a136f80866b0
adriandolha <adriandolha@yahoo.com>
2022-11-14 13:41:21 +0200

    unit tests

---
+++ django-pdf/tests/conftest.py
@@ -0,0 +1,56 @@
+import base64
+import json
+from unittest import mock
+from unittest.mock import patch
+
+import pytest
+
+
+def cache_get(key):
+    if key == 'admin#reports#avg_expenses_per_category':
+        return json.dumps(
+            {
+                'columns': ['country', 'category', 'amount'],
+                'items': [['Italy', 'apartment', '200']]
+            }
+        )
+    if key == 'admin#reports#expenses_per_month':
+        return json.dumps(
+            {
+                'columns': ['country', 'year', 'month', 'amount'],
+                'items': [['Italy', '2022', '1', '10000']]
+            }
+        )
+    return None
+
+
+@pytest.fixture()
+def cache_mock():
+    with mock.patch('django_pdf.views.cache') as _cache:
+        with mock.patch('django_pdf.seaborn_reports.cache') as _seaborn_cache:
+            _cache.get.side_effect = cache_get
+            _seaborn_cache.get.side_effect = cache_get
+            yield _cache
+
+
+@pytest.fixture()
+def weasyprint_mock():
+    with mock.patch('django_pdf.weasyprint_pdf.HTML') as _html:
+        yield _html
+
+
+@pytest.fixture()
+def selenium_webdriver_mock():
+    with mock.patch('django_pdf.selenium_pdf.webdriver') as _webdriver:
+        with mock.patch('django_pdf.selenium_pdf.WebDriverWait'):
+            with mock.patch('django_pdf.selenium_pdf.time'):
+                _chrome_driver = _webdriver.Chrome.return_value
+                _chrome_driver.execute_cdp_cmd.return_value = {"data": base64.b64encode(b"hi")}
+                yield _chrome_driver
+
+
+@pytest.fixture()
+def reportlab_mock():
+    with mock.patch('django_pdf.reportlab_pdf.get_doc') as _doc:
+        with mock.patch('django_pdf.reportlab_pdf.seaborn_reports'):
+            yield _doc
---
+++ django-pdf/tests/test_home.py
@@ -0,0 +1,68 @@
+from unittest import mock
+from unittest.mock import patch, MagicMock
+
+from django.test import Client, RequestFactory
+from django_pdf import views
+
+
+class TestHome:
+    def test_home_unauthorized(self):
+        client = Client()
+        response = client.get('/')
+        assert response.status_code == 302
+
+    def test_home_valid(self, cache_mock):
+        request = MagicMock()
+        res = views.home(request)
+        assert res.status_code == 200
+
+    def test_home_weasyprint_valid(self, cache_mock, weasyprint_mock):
+        request = MagicMock()
+        request.GET = {'download': 'weasyprint'}
+        res = views.home(request)
+        assert res.status_code == 200
+        assert weasyprint_mock.return_value.write_pdf.called
+
+    def test_home_selenium_valid(self, cache_mock, selenium_webdriver_mock):
+        request = MagicMock()
+        request.GET = {'download': 'selenium'}
+        res = views.home(request)
+        assert res.status_code == 200
+        assert selenium_webdriver_mock.execute_cdp_cmd.called
+
+    def test_home_reportlab_valid(self, cache_mock, reportlab_mock):
+        request = MagicMock()
+        request.GET = {'download': 'reportlab'}
+        res = views.home(request)
+        assert res.status_code == 200
+        assert reportlab_mock.return_value.build.called
+        assert len(reportlab_mock.return_value.build.call_args.args[0]) == 6
+
+    @mock.patch('django_pdf.views.view_expenses_chartsjs')
+    @mock.patch('django_pdf.views.view_expenses_seaborn')
+    def test_home_charts_query_param_chartsjs_valid(self, view_seaborn, view_chartsjs, cache_mock):
+        request = MagicMock()
+        request.GET = {'charts': 'chartsjs'}
+        res = views.home(request)
+        assert res.status_code == 200
+        assert view_chartsjs.called
+        assert not view_seaborn.called
+
+    @mock.patch('django_pdf.views.view_expenses_chartsjs')
+    @mock.patch('django_pdf.views.view_expenses_seaborn')
+    def test_home_charts_query_param_seaborn_valid(self, view_seaborn, view_chartsjs, cache_mock):
+        request = MagicMock()
+        request.GET = {'charts': 'seaborn'}
+        res = views.home(request)
+        assert res.status_code == 200
+        assert view_seaborn.called
+        assert not view_chartsjs.called
+
+    @mock.patch('django_pdf.views.view_expenses_chartsjs')
+    @mock.patch('django_pdf.views.view_expenses_seaborn')
+    def test_home_charts_query_param_default(self, view_seaborn, view_chartsjs, cache_mock):
+        request = MagicMock()
+        res = views.home(request)
+        assert res.status_code == 200
+        assert not view_chartsjs.called
+        assert view_seaborn.called
